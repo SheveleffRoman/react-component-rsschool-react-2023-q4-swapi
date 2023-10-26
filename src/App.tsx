b671f4f98d9e527c304a2e9578b0a93c5612dc3c
Roman <@>
2023-10-26 16:30:02 +0400

    feat: add app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { Component } from 'react';
+import Search from './Components/Search/Search';
 
-function App() {
-  const [count, setCount] = useState(0);
+export type SearchProps = {
+  onSearch: (searchTerm: string) => void;
+};
 
-  return (
-    <>
+export interface Planet {
+  name: string;
+  climate: string;
+  diameter: string;
+  rotation_period: string;
+  population: string;
+  terrain: string;
+  surface_water: string;
+}
+
+export type SearchResultsProps = {
+  searchResults: Planet[];
+  isLoading: boolean;
+  error: Error | null;
+};
+
+class App extends Component {
+  render() {
+    return (
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Search />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+    );
+  }
 }
 
 export default App;
