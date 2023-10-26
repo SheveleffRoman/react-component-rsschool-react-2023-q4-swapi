d2b22071e77a7d05b319b39b8af92c3df27532f3
Roman <@>
2023-10-26 16:37:44 +0400

    feat: add search component

---
+++ src/Components/Search/Search.tsx
@@ -0,0 +1,71 @@
+import { Component } from 'react';
+import Card from '../Card/Card';
+
+export default class Search extends Component {
+  state = {
+    storedSearch: '',
+    searchResults: [],
+    error: null,
+    isLoading: false,
+  };
+
+  componentDidMount() {
+    const storedValue = this.getStoredSearchTerm();
+    this.setState({ storedSearch: storedValue });
+    this.handleSearch(storedValue!);
+  }
+
+  getStoredSearchTerm = () => localStorage.getItem('searchTerm');
+
+  handleSearch = (searchTerm = '') => {
+    this.setState({ isLoading: true });
+    const apiUrl = searchTerm
+      ? `https://swapi.dev/api/planets/?search=${searchTerm}`
+      : 'https://swapi.dev/api/planets/';
+
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ searchResults: data.results, error: null });
+      })
+      .catch((error) => {
+        this.setState({ searchResults: [], error });
+        console.error(error);
+      })
+      .finally(() => {
+        this.setState({ isLoading: false });
+      });
+  };
+
+  handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ storedSearch: event.target.value });
+  };
+
+  search = () => {
+    localStorage.setItem('searchTerm', this.state.storedSearch.trim());
+    this.handleSearch(this.state.storedSearch.trim());
+  };
+
+  render() {
+    const { searchResults, isLoading } = this.state;
+
+    const cardComponents = searchResults.map((item, index) => (
+      <Card key={index} data={item} />
+    ));
+
+    return (
+      <div>
+        <input
+          value={this.state.storedSearch}
+          onChange={this.handleSearchChange}
+        />
+        <button onClick={this.search}>Search</button>
+        {isLoading ? (
+          <div className="loading">Loading...</div>
+        ) : (
+          <div className="search-results">{cardComponents}</div>
+        )}
+      </div>
+    );
+  }
+}
