cf815d6161f557aac92cc99b438ccec1e7cfc46f
Roman <@>
2023-11-09 18:56:02 +0400

    feat: add 404 page

--- src/Components/Error/NotFound.tsx
+++ src/Components/Error/NotFound.tsx
@@ -1,9 +1,4 @@
-import { useRouteError } from 'react-router-dom';
-
 export default function ErrorPage() {
-  const error = useRouteError();
-  console.error(error);
-
   return (
     <div className="error-message">
       <h2 className="title">Sorry, an unexpected error has occurred</h2>
