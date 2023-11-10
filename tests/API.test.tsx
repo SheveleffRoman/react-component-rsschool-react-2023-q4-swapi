1758b2a55773354210ac629d7b29dab0ca6745bd
Roman <@>
2023-11-10 12:48:19 +0400

    feat: add api test

---
+++ tests/API.test.tsx
@@ -0,0 +1,28 @@
+import { describe, it, expect } from 'vitest';
+import DataService from '../src/API/DataService';
+
+describe('DataService', () => {
+  it('gets all planets', async () => {
+    const search = 'tatooine';
+    const result = await DataService.getAll(search);
+    expect(result.status).toBe(200);
+  });
+
+  it('gets a planet by id', async () => {
+    const id = '1';
+    const result = await DataService.getById(id);
+    expect(result.status).toBe(200);
+  });
+
+  it('gets a planet by page url', async () => {
+    const url = 'https://swapi.dev/api/planets/?page=2';
+    const result = await DataService.getByPage(url);
+    expect(result.status).toBe(200);
+  });
+
+  it('gets residents by url', async () => {
+    const url = 'https://swapi.dev/api/people/1/';
+    const result = await DataService.getResidentsByUrl(url);
+    expect(result.status).toBe(200);
+  });
+});
