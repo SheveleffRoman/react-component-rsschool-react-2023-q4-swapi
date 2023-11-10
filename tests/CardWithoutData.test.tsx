e13c39fa5680197d90aac5b3da9d73bd0990b6e7
Roman <@>
2023-11-10 12:49:06 +0400

    feat: add empty data test

--- tests/CardWithoutData.test.tsx
+++ tests/CardWithoutData.test.tsx
@@ -6,7 +6,9 @@ import Main from '../src/Pages/Main/Main';
 import '@testing-library/jest-dom';
 
 vi.mock('../src/context/appContext.ts', async () => {
-  const actual = await vi.importActual('../src/context/appContext.ts');
+  const actual = (await vi.importActual(
+    '../src/context/appContext.ts'
+  )) as object;
   return {
     ...actual,
     useAppContext: vi.fn(() => ({
