305c0990d901b7b693f572ef611e0ff4f4316531
Roman <@>
2023-11-18 03:19:56 +0400

    refactor: delete unused file

--- tests/AppContext.test.tsx
+++
@@ -1,9 +0,0 @@
-import { expect, test, vi } from 'vitest';
-import { useAppContext } from '../src/context/appContext.ts';
-import React from 'react';
-
-test('throws error if context undefined', () => {
-  vi.spyOn(React, 'useContext').mockReturnValue(undefined);
-
-  expect(() => useAppContext()).toThrowError();
-});
