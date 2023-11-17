35a06247c51a170d885c9acac1f225f801ee8e0e
Roman <@>
2023-11-18 03:18:45 +0400

    feat: add msw lib and config server with handler

---
+++ tests/setup.ts
@@ -0,0 +1,11 @@
+import { afterAll, afterEach, beforeAll } from 'vitest';
+import { cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/vitest';
+import { server } from '../src/mocks/server';
+
+beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
+afterAll(() => server.close());
+afterEach(() => {
+  server.resetHandlers();
+  cleanup();
+});
