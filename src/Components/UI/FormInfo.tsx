8167f4aeadacc73e3f8e284bf5b09f6070fcf0b8
SheveleffRoman <@>
2023-12-01 15:43:57 +0400

    feat: add form info component

---
+++ src/Components/UI/FormInfo.tsx
@@ -0,0 +1,29 @@
+import { IStore } from '../../app.interface';
+
+const FormInfo = ({ form }: { form: IStore }) => {
+  return (
+    <div className="form-info">
+      <h3>{form.name}</h3>
+      <div>
+        <b>Age:</b> {form.age}
+      </div>
+      <div>
+        <b>Email:</b> {form.email}
+      </div>
+      <div>
+        <b>Gender:</b> {form.gender}
+      </div>
+      <div>
+        <b>Country:</b> {form.country}
+      </div>
+      <div>
+        <b>Accepted terms:</b> {form.terms ? 'Yes' : 'No'}
+      </div>
+      <div>
+        <b>Image Base64:</b> {form.image.slice(0, 30)} ...
+      </div>
+    </div>
+  );
+};
+
+export default FormInfo;
