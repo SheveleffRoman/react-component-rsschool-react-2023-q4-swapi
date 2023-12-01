210bf5c05b2e5b3c93941652bd774755c7756422
SheveleffRoman <@>
2023-12-01 15:44:39 +0400

    feat: show collection of submit data

--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -1,7 +1,10 @@
 import { Link } from 'react-router-dom';
 import '../../App.css';
+import { useAppSelector } from '../hooks/redux';
+import FormInfo from '../UI/FormInfo';
 
 function Main() {
+  const { collection } = useAppSelector((state) => state.collectSubmit);
   return (
     <>
       <h1>Main Route </h1>
@@ -11,6 +14,9 @@ function Main() {
       <Link to={'/uncontrolled'}>
         <button>to uncontrolled</button>
       </Link>
+
+      {collection &&
+        collection.map((form) => <FormInfo form={form} key={form.name} />)}
     </>
   );
 }
