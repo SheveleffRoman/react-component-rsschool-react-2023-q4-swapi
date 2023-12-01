b8122cf5b71d3d2da27a862a262447f9e2176b61
SheveleffRoman <@>
2023-12-01 15:57:05 +0400

    refactor: extract Yup schema into separate file

--- src/Components/ReactHookForm/ReactHookForm.tsx
+++ src/Components/ReactHookForm/ReactHookForm.tsx
@@ -4,65 +4,8 @@ import { FormData } from '../../app.interface';
 import { useAppDispatch, useAppSelector } from '../hooks/redux';
 import { dataSlice } from '../../store/reducers/dataSlice';
 import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
-import YupPassword from 'yup-password';
 import { convertToBase64 } from '../converterBase64';
-
-YupPassword(yup);
-
-const schema = yup
-  .object({
-    name: yup
-      .string()
-      .required()
-      .matches(/^[A-Za-zА-ЯЁа-яё][A-Za-zА-ЯЁа-яё0-9\s-]*$/, {
-        message: 'Invalid name format',
-        excludeEmptyString: true,
-      })
-      .test(
-        'startsWithCapital',
-        'Name should start with a capital letter',
-        (value) => {
-          if (value) {
-            const firstChar = value.charAt(0);
-            return /^[A-ZА-ЯЁ]/.test(firstChar);
-          }
-          return true;
-        }
-      ),
-    age: yup
-      .number()
-      .transform((value) => (Number.isNaN(value) ? null : value))
-      .nullable()
-      .positive()
-      .required(),
-    email: yup.string().email().required(),
-    password: yup.string().password().required(),
-    confirmPassword: yup
-      .string()
-      .oneOf([yup.ref('password'), undefined], 'Passwords must match')
-      .required('Password confirmation is required'),
-    gender: yup.string().required('Gender is required'),
-    terms: yup.boolean().oneOf([true], 'Agree is required').required(),
-    image: yup
-      .mixed<FileList>()
-      .required('File not found')
-      .test('fileExists', 'File not found', (files) => {
-        return !files || files.length != 0;
-      })
-      .test('fileFormat', 'Only JPG or PNG files are allowed', (files) => {
-        return Array.from(files).every(
-          (file) =>
-            file.name.toLowerCase().endsWith('.jpg') ||
-            file.name.toLowerCase().endsWith('.png')
-        );
-      })
-      .test('fileSize', 'Only documents up to 2MB are permitted', (files) => {
-        return Array.from(files).every((file) => file.size <= 2_000_000);
-      }),
-    country: yup.string().required(),
-  })
-  .required();
+import { schema } from '../../schema/schema';
 
 function RHF() {
   const { countries } = useAppSelector((state) => state.countrySlice);
---
+++ src/schema/schema.ts
@@ -0,0 +1,58 @@
+import YupPassword from 'yup-password';
+import * as yup from 'yup';
+
+YupPassword(yup);
+
+export const schema = yup
+  .object({
+    name: yup
+      .string()
+      .required()
+      .matches(/^[A-Za-zА-ЯЁа-яё][A-Za-zА-ЯЁа-яё0-9\s-]*$/, {
+        message: 'Invalid name format',
+        excludeEmptyString: true,
+      })
+      .test(
+        'startsWithCapital',
+        'Name should start with a capital letter',
+        (value) => {
+          if (value) {
+            const firstChar = value.charAt(0);
+            return /^[A-ZА-ЯЁ]/.test(firstChar);
+          }
+          return true;
+        }
+      ),
+    age: yup
+      .number()
+      .transform((value) => (Number.isNaN(value) ? null : value))
+      .nullable()
+      .positive()
+      .required(),
+    email: yup.string().email().required(),
+    password: yup.string().password().required(),
+    confirmPassword: yup
+      .string()
+      .oneOf([yup.ref('password'), undefined], 'Passwords must match')
+      .required('Password confirmation is required'),
+    gender: yup.string().required('Gender is required'),
+    terms: yup.boolean().oneOf([true], 'Agree is required').required(),
+    image: yup
+      .mixed<FileList>()
+      .required('File not found')
+      .test('fileExists', 'File not found', (files) => {
+        return !files || files.length != 0;
+      })
+      .test('fileFormat', 'Only JPG or PNG files are allowed', (files) => {
+        return Array.from(files).every(
+          (file) =>
+            file.name.toLowerCase().endsWith('.jpg') ||
+            file.name.toLowerCase().endsWith('.png')
+        );
+      })
+      .test('fileSize', 'Only documents up to 2MB are permitted', (files) => {
+        return Array.from(files).every((file) => file.size <= 2_000_000);
+      }),
+    country: yup.string().required(),
+  })
+  .required();
